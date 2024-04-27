84019b8284ea9cff0d3af82ce654cc0949e59560
Kim Da Eun <midekuna@gmail.com>
2024-04-27 16:14:01 +0900

    chore: sync code with step2

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,6 @@ import './index.css';
 import { Outlet } from 'react-router-dom';
 
 function App() {
-  const [cardInfo, setCardInfo] = useState<CardInfo>({
-    cardNumbers: ['', '', '', ''],
-    expirationDate: ['', ''],
-    ownerName: '',
-  });
-
-  const setCardData = (
-    key: keyof CardInfo,
-    newData: CardInfo[keyof CardInfo]
-  ) => {
-    setCardInfo({ ...cardInfo, [key]: newData });
-  };
-
   return (
     <div className="app">
       <Outlet />
